ced11e4e339db51c3c635f5cdb02ed5410cf991d
ankhod <ankhod7@gmail.com>
2025-04-13 20:46:38 +0300

    createSelector for memoization

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,12 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
-export const selectFilteredContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = state.filters.name;
-  return contacts.filter(
-    contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-      contact.number.toLowerCase().includes(filter.toLowerCase())
-  );
-};
+
+export const selectNameFilter = state => state.filters.name;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
